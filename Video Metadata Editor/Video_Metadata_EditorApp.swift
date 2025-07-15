ec6410cde1baee761be2f53de1c2f62e91a18653
Luces Umbrarum <gm.konsortium@yahoo.dk>
2025-07-15 17:50:16 +0200

    Initial Commit

---
+++ Video Metadata Editor/Video_Metadata_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_Metadata_EditorApp.swift
+//  Video Metadata Editor
+//
+//  Created by simMAX on 15.07.25.
+//
+
+import SwiftUI
+
+@main
+struct Video_Metadata_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Video Metadata EditorTests/Video_Metadata_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_Metadata_EditorTests.swift
+//  Video Metadata EditorTests
+//
+//  Created by simMAX on 15.07.25.
+//
+
+import Testing
+@testable import Video_Metadata_Editor
+
+struct Video_Metadata_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
